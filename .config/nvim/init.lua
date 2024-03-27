13486e691464faa9c24989b13f152534b52c7110
mosubpink <senkuchansd@gmail.com>
2024-03-27 22:57:51 +0200

    adding lazy.vim configs

---
+++ .config/nvim/init.lua
@@ -0,0 +1,53 @@
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.g.mapleader = '<space>'
+-- buffer-scoped
+vim.opt.autoindent = true
+-- window-scoped
+vim.opt.cursorline = true
+-- global scope
+vim.opt.autowrite = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+local lazy = {}
+
+function lazy.install(path)
+  if not vim.loop.fs_stat(path) then
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      path,
+    })
+  end
+end
+
+function lazy.setup(plugins)
+  if vim.g.plugins_ready then
+    return
+  end
+
+  -- You can "comment out" the line below after lazy.nvim is installed
+  --lazy.install(lazy.path)
+
+  vim.opt.rtp:prepend(lazy.path)
+
+  require('lazy').setup(plugins, lazy.opts)
+  vim.g.plugins_ready = true
+end
+lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+lazy.opts = {}
+lazy.setup({
+  {import = 'core.plugins'},
+  {import = 'core.themes'}
+})
+vim.opt.termguicolors = true
+vim.cmd([[colorscheme oh-lucy-evening]])
