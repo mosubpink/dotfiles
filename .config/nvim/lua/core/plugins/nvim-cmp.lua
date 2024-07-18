d0ef3da07942856ae98d9fdaed6216a7a47bf256
blank <mosubpink@proton.me>
2024-07-18 22:59:08 +0200

    update

--- .config/nvim/lua/core/plugins/nvim-cmp.lua
+++ .config/nvim/lua/core/plugins/nvim-cmp.lua
@@ -15,11 +15,11 @@ config = function()
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
         -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+        vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
       end,
     },
     window = {
@@ -37,7 +37,7 @@ config = function()
       { name = 'nvim_lsp' },
       -- { name = 'vsnip' }, -- For vsnip users.
       {  name = 'path',},
-      { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
       -- { name = 'ultisnips' }, -- For ultisnips users.
       -- { name = 'snippy' }, -- For snippy users.
     }, {
