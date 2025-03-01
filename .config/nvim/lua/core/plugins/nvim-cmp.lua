395005f70170c52d93e1300c3ae974a13e5a5094
blank <mosubpink@proton.me>
2025-03-01 10:05:32 +0200

    add luarocks path to lua-lsp

--- .config/nvim/lua/core/plugins/nvim-cmp.lua
+++ .config/nvim/lua/core/plugins/nvim-cmp.lua
@@ -80,7 +80,28 @@ config = function()
     capabilities = capabilities
   }
   require('lspconfig')['lua_ls'].setup {
-    capabilities = capabilities
+    capabilities = capabilities;
+        settings = {
+    Lua = {
+      runtime = {
+        version = 'Lua 5.4',
+        path = {
+          '?.lua',
+          '?/init.lua',
+          vim.fn.expand'~/.luarocks/share/lua/5.4/?.lua',
+          vim.fn.expand'~/.luarocks/share/lua/5.4/?/init.lua',
+          '/usr/share/5.4lua',
+          '/usr/share/lua/5.4/?/init.lua'
+        }
+      },
+      workspace = {
+        library = {
+          vim.fn.expand'~/.luarocks/share/lua/5.4',
+          '/usr/share/lua/5.4'
+        }
+      }
+    }
+  }
   }
   require('lspconfig')['clangd'].setup {
     capabilities = capabilities
